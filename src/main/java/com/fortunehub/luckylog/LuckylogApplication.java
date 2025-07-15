543129c539130ca28d78647e43efffb076707ee3
HJ0216 <6120hj@gmail.com>
2025-07-15 20:42:02 +0900

    🎉Init: #-- 프로젝트 시작

---
+++ src/main/java/com/fortunehub/luckylog/LuckylogApplication.java
@@ -0,0 +1,13 @@
+package com.fortunehub.luckylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckylogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fortunehub/luckylog/LuckylogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortunehub.luckylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckylogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
