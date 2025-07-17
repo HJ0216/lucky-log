41957fc3176c340d1ef2e2f23d79afad7164641a
HJ0216 <6120hj@gmail.com>
2025-07-17 21:52:01 +0900

    📝Docs: #-- 유저 API에 대한 Swagger 문서 추가

--- src/main/java/com/fortunehub/luckylog/dto/response/UserResponse.java
+++ src/main/java/com/fortunehub/luckylog/dto/response/UserResponse.java
@@ -1,8 +1,13 @@
 package com.fortunehub.luckylog.dto.response;
 
 import com.fortunehub.luckylog.domain.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 
-public record UserResponse(String email, String nickname) {
+public record UserResponse(
+    @Schema(description = "사용자 로그인 이메일", example = "chrome123@naver.com")
+    String email,
+    @Schema(description = "서비스에서 사용할 별칭", example = "nick")
+    String nickname) {
 
   public static UserResponse from(User user) {
     return new UserResponse(user.getEmail(), user.getNickname());
