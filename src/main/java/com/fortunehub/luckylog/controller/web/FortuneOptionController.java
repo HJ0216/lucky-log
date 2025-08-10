3a70f9a36767cd7f0e18fdfe571a50689d8d9fda
HJ0216 <6120hj@gmail.com>
2025-08-10 12:01:43 +0900

    ✨Feat: #2 운세 보기 버튼 클릭 시, 데이터 전달 로직 추가

--- src/main/java/com/fortunehub/luckylog/controller/web/FortuneOptionController.java
+++ src/main/java/com/fortunehub/luckylog/controller/web/FortuneOptionController.java
@@ -2,11 +2,16 @@ package com.fortunehub.luckylog.controller.web;
 
 import com.fortunehub.luckylog.form.BirthInfoForm;
 import com.fortunehub.luckylog.form.FortuneOptionForm;
+import jakarta.validation.Valid;
+import java.util.LinkedHashSet;
+import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -17,8 +22,53 @@ public class FortuneOptionController {
   @GetMapping
   public String show(@ModelAttribute BirthInfoForm birthInfo, Model model) {
 
-    model.addAttribute("fortuneOptionsForm", new FortuneOptionForm());
+    model.addAttribute("fortuneOptionForm", new FortuneOptionForm());
 
     return "fortune-option";
+  }
+
+  @PostMapping
+  public String submit(
+      @Valid @ModelAttribute BirthInfoForm birthInfoForm,
+      @Valid @ModelAttribute FortuneOptionForm fortuneOptionForm,
+      BindingResult result,
+      Model model
+  ) throws Exception {
+
+    log.debug("사용자 정보 상세: 달력={}, 성별={}, 년도={}, 월={}, 일={}, 시간={}, 도시={}",
+        birthInfoForm.getCalendar(),
+        birthInfoForm.getGender(),
+        birthInfoForm.getYear(),
+        birthInfoForm.getMonth(),
+        birthInfoForm.getDay(),
+        birthInfoForm.getTime(),
+        birthInfoForm.getCity());
+
+    log.debug("운세 옵션 상세: ai={}, 종류={}, 기간별={}",
+        fortuneOptionForm.getAi(),
+        fortuneOptionForm.getFortunes(),
+        fortuneOptionForm.getPeriod());
+
+    if (result.hasErrors()) {
+      Set<String> errorMessages = new LinkedHashSet<>();
+      Set<String> errorFields = new LinkedHashSet<>();
+
+      result.getFieldErrors().forEach(error -> {
+        errorMessages.add(error.getDefaultMessage());
+        errorFields.add(error.getField());
+      });
+
+      model.addAttribute("errorMessages", errorMessages);
+      model.addAttribute("errorFields", errorFields);
+
+      return "fortune-option";
+    }
+
+    // TODO: ai 연결
+
+    model.addAttribute("birthInfo", birthInfoForm);
+    model.addAttribute("fortuneOptionForm", new FortuneOptionForm());
+
+    return "fortune-result";
   }
 }
