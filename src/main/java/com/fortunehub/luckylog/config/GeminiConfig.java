53dbf7ca07a208214da123d54432742c46986127
HJ0216 <6120hj@gmail.com>
2025-08-16 12:37:05 +0900

    ✨Feat: #7 모델 추가 설정 및 로깅 출력 내용 변경

--- src/main/java/com/fortunehub/luckylog/config/GeminiConfig.java
+++ src/main/java/com/fortunehub/luckylog/config/GeminiConfig.java
@@ -1,6 +1,7 @@
 package com.fortunehub.luckylog.config;
 
 import com.google.genai.Client;
+import com.google.genai.types.GenerateContentConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +13,17 @@ public class GeminiConfig {
   private String apiKey;
 
   @Bean
-  public Client geminiClient() {
+  public Client client() {
     return new Client.Builder()
         .apiKey(apiKey)
         .build();
+  }
+
+  @Bean
+  public GenerateContentConfig generateContentConfig() {
+    return GenerateContentConfig.builder()
+                                .temperature(0.2f) // 창의성
+                                .topP(0.9f) // 다양성
+                                .build();
   }
 }
