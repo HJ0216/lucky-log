0fdeb5b7f2cb329e20cc71393a11b4120ef6ada3
HJ0216 <6120hj@gmail.com>
2025-11-06 12:30:53 +0900

    ✨Feat: #9 security 관련 설정 활성화

--- src/main/java/com/fortunehub/luckylog/config/SecurityConfig.java
+++ src/main/java/com/fortunehub/luckylog/config/SecurityConfig.java
@@ -3,19 +3,22 @@ package com.fortunehub.luckylog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
         .csrf(csrf -> csrf.disable())
+        .formLogin(form -> form.disable())
         .headers(headers -> headers
-            .frameOptions(frameOptions -> frameOptions.disable())
+            .frameOptions(frameOptions -> frameOptions.disable()) // h2 db 관련 설정
         );
     return http.build();
   }
