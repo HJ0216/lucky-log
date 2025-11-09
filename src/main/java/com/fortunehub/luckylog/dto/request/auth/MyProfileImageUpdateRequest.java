56d452bf27d5adfbb7167b520903f211546377d6
HJ0216 <6120hj@gmail.com>
2025-11-09 19:34:45 +0900

    🔥remove: #-- api 관련 내용 삭제

--- src/main/java/com/fortunehub/luckylog/dto/request/auth/MyProfileImageUpdateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.fortunehub.luckylog.dto.request.auth;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-public record MyProfileImageUpdateRequest(
-    @Schema(description = "사용자 프로필 이미지 url", example = "https://mybucket.s3.amazonaws.com/profiles/user-1-profile.jpg")
-    String url) {
-
-}
