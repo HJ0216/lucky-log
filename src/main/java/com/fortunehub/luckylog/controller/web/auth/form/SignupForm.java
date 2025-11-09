ed09c46e3362a92a61fcf0696c12ff431623f927
HJ0216 <6120hj@gmail.com>
2025-11-10 08:35:58 +0900

    🐛Fix: #-- 이메일 형식 및 닉네임 형식 체크 오류 수정

--- src/main/java/com/fortunehub/luckylog/controller/web/auth/form/SignupForm.java
+++ src/main/java/com/fortunehub/luckylog/controller/web/auth/form/SignupForm.java
@@ -1,6 +1,5 @@
 package com.fortunehub.luckylog.controller.web.auth.form;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -12,7 +11,10 @@ import lombok.NoArgsConstructor;
 public class SignupForm {
 
   @NotBlank(message = "📧 이메일을 입력해주세요!")
-  @Email(message = "📧 올바른 이메일 형식이 아닙니다")
+  @Pattern(
+      regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$",
+      message = "📧 올바른 이메일 형식이 아닙니다"
+  )
   private String email;
 
   @NotBlank(message = "🔒 비밀번호를 입력해주세요!")
@@ -26,6 +28,7 @@ public class SignupForm {
   @NotBlank(message = "🔐 비밀번호 확인을 입력해주세요!")
   private String confirmPassword;
 
-  @Size(min = 2, max = 20, message = "✨ 닉네임은 2-20자 사이여야 합니다!")
+  // optional, 입력 시 2-20자 사이
+  @Pattern(regexp = "^$|^.{2,20}$", message = "✨ 닉네임은 2-20자 사이여야 합니다!")
   private String nickname;
 }
