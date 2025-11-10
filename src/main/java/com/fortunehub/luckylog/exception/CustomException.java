0a06127225571b2ab1cd4065ff9874d60c236d48
HJ0216 <6120hj@gmail.com>
2025-11-10 10:00:24 +0900

    🧹Chore: #36 Lombok 어노테이션으로 변경

--- src/main/java/com/fortunehub/luckylog/exception/CustomException.java
+++ src/main/java/com/fortunehub/luckylog/exception/CustomException.java
@@ -1,15 +1,14 @@
 package com.fortunehub.luckylog.exception;
 
+import lombok.Getter;
+
+@Getter
 public class CustomException extends RuntimeException {
 
   private final ErrorCode errorCode;
 
   public CustomException(ErrorCode errorCode) {
-    super(errorCode.getMessage());
+    super(errorCode.getMessage()); // getMessage()로 예외 메시지를 불러올 수 있게 하는 역할
     this.errorCode = errorCode;
-  }
-
-  public ErrorCode getErrorCode() {
-    return errorCode;
   }
 }
