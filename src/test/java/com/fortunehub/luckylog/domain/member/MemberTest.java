45e58ae4bbabba294fd4c91924973e00fe9b8a6d
HJ0216 <6120hj@gmail.com>
2025-11-11 07:41:34 +0900

    ✅Test: #-- 회원 Entity 테스트 추가

---
+++ src/test/java/com/fortunehub/luckylog/domain/member/MemberTest.java
@@ -0,0 +1,37 @@
+package com.fortunehub.luckylog.domain.member;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.fortunehub.luckylog.dto.request.auth.SignupRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("회원 Entity")
+class MemberTest {
+
+  @Test
+  @DisplayName("이메일에 대문자가 있을 경우, 소문자로 변환한다")
+  void ToLowerCaseNormalizeEmail(){
+    // given
+    SignupRequest request = new SignupRequest("EMAIL@EMAIL.COM", "PassWord147@", "솜사탕");
+
+    // when
+    Member member = request.toEntity("EncodedPassword147@");
+
+    // then
+    assertEquals("email@email.com", member.getEmail());
+  }
+
+  @Test
+  @DisplayName("이메일 앞 또는 뒤에 공백이 있을 경우 제거한다")
+  void TrimNormalizeEmail(){
+    // given
+    SignupRequest request = new SignupRequest("  email@email.com  ", "PassWord147@", "솜사탕");
+
+    // when
+    Member member = request.toEntity("EncodedPassword147@");
+
+    // then
+    assertEquals("email@email.com", member.getEmail());
+  }
+}
