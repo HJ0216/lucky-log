eab875f036b6fc9c213ecfc9c64b9d66459a3096
HJ0216 <6120hj@gmail.com>
2025-11-23 20:01:15 +0900

    🧹Chore: #-- local 환경에서 테스트 유저 자동 추가 설정

--- src/main/java/com/fortunehub/luckylog/init/DataInitializer.java
+++ src/main/java/com/fortunehub/luckylog/init/DataInitializer.java
@@ -2,12 +2,15 @@ package com.fortunehub.luckylog.init;
 
 import com.fortunehub.luckylog.domain.fortune.FortuneCategory;
 import com.fortunehub.luckylog.domain.fortune.FortuneType;
+import com.fortunehub.luckylog.domain.member.Member;
 import com.fortunehub.luckylog.repository.fortune.FortuneCategoryRepository;
+import com.fortunehub.luckylog.repository.member.MemberRepository;
 import jakarta.annotation.PostConstruct;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,14 +20,23 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class DataInitializer {
 
-  private final FortuneCategoryRepository repository;
+  private final FortuneCategoryRepository fortuneCategoryRepository;
+  private final MemberRepository memberRepository;
+  private final PasswordEncoder passwordEncoder;
 
   @PostConstruct
   @Transactional
   public void init() {
-    if (repository.count() > 0) return;
+    initFortuneCategories();
+    initMembers();
+  }
 
-    repository.saveAll(List.of(
+  private void initFortuneCategories() {
+    if (fortuneCategoryRepository.count() > 0) {
+      return;
+    }
+
+    fortuneCategoryRepository.saveAll(List.of(
         FortuneCategory.create(FortuneType.OVERALL),
         FortuneCategory.create(FortuneType.MONEY),
         FortuneCategory.create(FortuneType.LOVE),
@@ -33,6 +45,17 @@ public class DataInitializer {
         FortuneCategory.create(FortuneType.LUCK),
         FortuneCategory.create(FortuneType.FAMILY),
         FortuneCategory.create(FortuneType.HEALTH)
+    ));
+  }
+
+  private void initMembers() {
+    if (memberRepository.count() > 0) {
+      return;
+    }
+
+    memberRepository.saveAll(List.of(
+        new Member("lucky@email.com", passwordEncoder.encode("password@123"), "솜사탕 구름"),
+        new Member("test@email.com", passwordEncoder.encode("test@123"), "테스트 유저")
     ));
   }
 }
