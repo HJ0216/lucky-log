ad0efc2197078fd2e435fd59d2e42707fd784074
HJ0216 <6120hj@gmail.com>
2025-12-15 21:14:25 +0900

    🧹Chore #59 사용하지 않는 import문 정리

--- src/test/java/com/fortunehub/luckylog/controller/api/fortune/ExternalFortuneControllerTest.java
+++ src/test/java/com/fortunehub/luckylog/controller/api/fortune/ExternalFortuneControllerTest.java
@@ -2,7 +2,6 @@ package com.fortunehub.luckylog.controller.api.fortune;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
